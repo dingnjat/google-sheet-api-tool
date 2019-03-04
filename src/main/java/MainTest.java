c85d0492e0fdd998b37bdd33cc0ae3a90397ff90
dingnjat <annanshakai@gmail.com>
2019-03-04 12:20:58 +0700

    mm

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,15 +1,33 @@
-import com.google.api.services.sheets.v4.Sheets;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import model.User;
 import utils.SheetsServiceUtil;
-import utils.SpreadsheetTools;
+import utils.SpreadsheetUtil;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainTest {
 
+    private static final String SPREADSHEET_ID = "1z8NlBfxH330BxI79M8bxHPvYZqpxy8HEYjKmWkLo-Wc";
+
     public static void main(String[] args) throws GeneralSecurityException, IOException {
-        Sheets service = SheetsServiceUtil.getSheetsSerivce();
-        SpreadsheetTools spreadsheetTools = new SpreadsheetTools(service);
-        spreadsheetTools.create("sheets");
+        SpreadsheetUtil spreadsheetUtil = new SpreadsheetUtil(SheetsServiceUtil.getSheetsSerivce());
+        Gson gson = new Gson();
+        List<User> users = new ArrayList<>();
+        users.add(new User("john", "san diago", 21));
+        users.add(new User("thomas", "miami", 22));
+        String jsonString = gson.toJson(users);
+        List<User> userList = gson.fromJson(jsonString, new TypeToken<List<User>>() {
+        }.getType());
+        List<List<Object>> result = new ArrayList<>();
+        for (User user : userList) {
+            List<Object> t = Arrays.asList(user.toValues());
+            result.add(t);
+        }
+        spreadsheetUtil.updateValues(SPREADSHEET_ID, "Sheet1", "RAW", result);
     }
 }
--- src/main/java/utils/SpreadsheetUtil.java
+++ src/main/java/utils/SpreadsheetUtil.java
@@ -1,21 +1,18 @@
 package utils;
 
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.Spreadsheet;
-import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
-import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
-import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.api.services.sheets.v4.model.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SpreadsheetTools {
+public class SpreadsheetUtil {
 
     private Sheets service;
 
-    public SpreadsheetTools(Sheets service) {
+    public SpreadsheetUtil(Sheets service) {
         this.service = service;
     }
 
@@ -27,9 +24,7 @@ public class SpreadsheetTools {
 
     public UpdateValuesResponse updateValues(String spreadsheetId, String range, String valueInputOption, List<List<Object>> values) throws IOException {
         ValueRange body = new ValueRange().setValues(values);
-        return service.spreadsheets().values().update(spreadsheetId, range, body)
-                .setValueInputOption(valueInputOption)
-                .execute();
+        return service.spreadsheets().values().update(spreadsheetId, range, body).setValueInputOption(valueInputOption).execute();
     }
 
     public UpdateValuesResponse updateValues(String spreadsheetId, String range, String valueInputOption, Object[][] values) throws IOException {
@@ -38,5 +33,18 @@ public class SpreadsheetTools {
             list.add(Arrays.asList(array));
         }
         return this.updateValues(spreadsheetId, range, valueInputOption, list);
+    }
+
+    public AppendValuesResponse appendValues(String spreadsheetId, String range, String valueInputOption, List<List<Object>> values) throws IOException {
+        ValueRange body = new ValueRange().setValues(values);
+        return service.spreadsheets().values().append(spreadsheetId, range, body).setValueInputOption(valueInputOption).execute();
+    }
+
+    public AppendValuesResponse appendValues(String spreadsheetId, String range, String valueInputOption, Object[][] values) throws IOException {
+        List<List<Object>> list = new ArrayList<>();
+        for (Object[] array : values) {
+            list.add(Arrays.asList(array));
+        }
+        return this.appendValues(spreadsheetId, range, valueInputOption, list);
     }
 }
